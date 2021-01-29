fe91710c1b9c4a7fed98b31cf377bc3a6adc9595
Ryan Campbell <ryanc123198@gmail.com>
2021-01-29 13:18:04 -0600

    cleaned up config.h, removed broken patches

--- config.h
+++ config.h
@@ -1,14 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 4;        /* border pixel of windows */
-static const unsigned int gappx     = 15;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "DejaVu Sans Mono Nerd Font:size=18", "DejaVu Sans Mono Nerd Font:size=24" };
-
+static const unsigned int borderpx 		= 4;        /* border pixel of windows */
+static const unsigned int gappx     	= 15;       /* gaps between windows */
+static const unsigned int snap      	= 32;       /* snap pixel */
+static const int swallowfloating    	= 0;        /* 1 means swallow floating windows by default */
+static const int showbar            	= 1;        /* 0 means no bar */
+static const int topbar             	= 1;        /* 0 means bottom bar */
+static const char *fonts[]  	        = { "DejaVu Sans Mono Nerd Font:size=18", "DejaVu Sans Mono Nerd Font:size=24" };
 
 /* // gruvbox blue */ 
 /* static const char col_normalbg[]      = "#076678"; */
@@ -18,7 +17,7 @@ static const char *fonts[]          = { "DejaVu Sans Mono Nerd Font:size=18", "D
 /* static const char col_selectfg[]      = "#282828"; */
 /* static const char col_selectbd[]      = "#076678"; */
 
-// monochrome 
+// monochrome dark
 /* static const char col_normalbg[]      = "#0a0a0a"; */
 /* static const char col_normalfg[]      = "#c9c9c9"; */
 /* static const char col_normalbd[]      = "#0a0a0a"; */
@@ -26,20 +25,28 @@ static const char *fonts[]          = { "DejaVu Sans Mono Nerd Font:size=18", "D
 /* static const char col_selectfg[]      = "#0a0a0a"; */
 /* static const char col_selectbd[]      = "#c9c9c9"; */
 
+// monochrome light 
+static const char col_normalbg[]      = "#f1f1f1"; 
+static const char col_normalfg[]      = "#212121"; 
+static const char col_normalbd[]      = "#f1f1f1"; 
+static const char col_selectbg[]      = "#212121"; 
+static const char col_selectfg[]      = "#f1f1f1"; 
+static const char col_selectbd[]      = "#20bbfc"; 
+
 // fav wal 
-static const char col_normalbg[]      = "#060815";
-static const char col_normalfg[]      = "#9fb9c1";
-static const char col_normalbd[]      = "#060815";
-static const char col_selectbg[]      = "#9fb9c1";
-static const char col_selectfg[]      = "#060815";
-static const char col_selectbd[]      = "#9fb9c1";
+/* static const char col_normalbg[]      = "#060815"; */
+/* static const char col_normalfg[]      = "#9fb9c1"; */
+/* static const char col_normalbd[]      = "#060815"; */
+/* static const char col_selectbg[]      = "#9fb9c1"; */
+/* static const char col_selectfg[]      = "#060815"; */
+/* static const char col_selectbd[]      = "#9fb9c1"; */
 
 static const char *colors[][3] = {
-	/*                            fg,           bg,  border   */
-	[SchemeNorm]    = { col_normalfg, col_normalbg,  col_normalbd},
-	[SchemeSel]     = { col_selectfg, col_selectbg,  col_selectbd},
-	[SchemeStatus]  = { col_normalfg, col_normalbg,  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
-	[SchemeTagsSel] = { col_selectfg, col_selectbg,  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
+		/*                  fg,           bg,            border   */
+		[SchemeNorm]    = { col_normalfg, col_normalbg,  col_normalbd},
+		[SchemeSel]     = { col_selectfg, col_selectbg,  col_selectbd},
+		[SchemeStatus]  = { col_normalfg, col_normalbg,  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
+		[SchemeTagsSel] = { col_selectfg, col_selectbg,  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
         [SchemeTagsNorm]= { col_normalfg, col_normalbg,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
         [SchemeInfoSel] = { col_normalfg, col_normalbg,  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
         [SchemeInfoNorm]= { col_normalfg, col_normalbg,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
@@ -54,14 +61,13 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	/* class            instance  title           tags mask  isfloating  isterminal   noswallow  monitor */
 	{ "Gimp",           NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Alacritty",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "kitty",          NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "St",             NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,             NULL,     "Picture-in-Picture",           0,         1,          0,           0,        -1 },
 	{ NULL,             NULL,     "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */
-	{ NULL,             NULL,     "steam", 0,         1,          0,           1,        -1 }, /* xev */
+	{ NULL,             NULL,     "Picture-in-Picture",           0,         1,          0,           0,        -1 },
 };
 
 /* layout(s) */
@@ -70,7 +76,7 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
-	/* symbol     arrange function */
+	/* symbol   arrange function */
 	{ "﩯",      tile },    /* first entry is default */
 	{ "缾",      NULL },    /* no layout function means floating behavior */
 	{ "",      monocle },
@@ -89,29 +95,30 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-c","-m", dmenumon, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
-static const char *webcmd[] = {"firefox", NULL};
-static const char *webprivatecmd[] = {"firefox", "--private-window", NULL};
-static const char *volup[] = {"dwm_volup", NULL};
-static const char *voldn[] = {"dwm_voldown", NULL};
-static const char *briup[] = {"dwm_brightnessup", NULL};
-static const char *bridn[] = {"ddwm_brightnessdown", NULL};
-static const char *audio[] = {"audio", NULL};
-static const char *net[] = {"networkmanager_dmenu", NULL};
-static const char *ss[] = {"screenshot", NULL};
-static const char *clipmenu[] = {"clipmenu", NULL};
-static const char *passmenu[] = {"bitwarden-dmenu", NULL};
-static const char *mount[] = {"dmenumount", NULL};
-static const char *dmenuexit[] = {"dmenuexit", NULL};
-static const char *emoji[] = {"emojenu", NULL};
+static const char *dmenucmd[] 		= { "dmenu_run", "-c","-m", dmenumon, NULL };
+static const char *termcmd[]  		= { "alacritty", NULL };
+static const char *webcmd[] 		= {"firefox", NULL};
+static const char *webprivatecmd[] 	= {"firefox", "--private-window", NULL};
+static const char *volup[] 			= {"dwm_volup", NULL};
+static const char *voldn[] 			= {"dwm_voldown", NULL};
+static const char *briup[]			= {"dwm_brightnessup", NULL};
+static const char *bridn[] 			= {"ddwm_brightnessdown", NULL};
+static const char *audio[] 			= {"audio", NULL};
+static const char *net[] 			= {"networkmanager_dmenu", NULL};
+static const char *ss[] 			= {"screenshot", NULL};
+static const char *clipmenu[] 		= {"clipmenu", NULL};
+static const char *passmenu[] 		= {"passmenu", NULL};
+static const char *mount[] 			= {"dmenumount", NULL};
+static const char *dmenuexit[] 		= {"dmenuexit", NULL};
+static const char *emoji[] 			= {"emojenu", NULL};
+
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_space,  spawn,          {.v = clipmenu } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-        { MODKEY|ShiftMask,             XK_w,      spawn,          {.v = webcmd} },
-        { MODKEY|ShiftMask,             XK_n,      spawn,          {.v = webprivatecmd} },
+    { MODKEY|ShiftMask,             XK_w,      spawn,          {.v = webcmd} },
+    { MODKEY|ShiftMask,             XK_n,      spawn,          {.v = webprivatecmd} },
 	{ MODKEY,                       XK_F12,    spawn,          {.v = volup } },
 	{ MODKEY,                       XK_F11,    spawn,          {.v = voldn } },
 	{ MODKEY,                       XK_F1,     spawn,          {.v = bridn } },
@@ -169,4 +176,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
